592d55d142bf64bc07bfe057e47285dd98e5d74d
marvin espira <marvinespira@adalabsafrica.com>
2021-09-18 11:36:12 +0300

    fix: swagger schema definitions

--- server/routes/api/client.js
+++ server/routes/api/client.js
@@ -1,9 +1,48 @@
 const { Router } = require('express')
 const Client = require('../../models/Client')
-const clientsDoc = require('../docs/clientsDocSchema')
 const router = Router()
 
-console.log('iiiiiiiiiiiiiiiiiii', clientsDoc)
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Client:
+ *       type: object
+ *       example:
+ *         id: 2e34wrf7
+ *         name: client test
+ *         email: clienttest@gmail.com
+ *         phone: 8175698909
+ *         providers: [ { id: skdfsdf }, { id: ksdzkw43} ]
+ *       required:
+ *         - name
+ *         - email
+ *         - phone
+ *         - providers
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the client
+ *         name:
+ *           type: string
+ *           description: The client name
+ *         email:
+ *           type: string
+ *           description: The client email address
+ *         phone:
+ *           type: integer
+ *           description: The client phone number
+ *         providers:
+ *           type: array
+ *           description: The client's providers
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Clients
+ *   description: The clients managing API
+ */
 
 /**
  * @swagger
--- server/routes/api/provider.js
+++ server/routes/api/provider.js
@@ -1,8 +1,33 @@
 const { Router } = require('express')
 const Provider = require('../../models/Provider')
-const providersDocs = require('../docs/providersDocSchema')
-
 const router = Router()
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Provider:
+ *       type: object
+ *       example:
+ *         id: sdfki4535e23
+ *         name: provider test
+ *       required:
+ *         - name
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the provider
+ *         name:
+ *           type: string
+ *           description: The provider name
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Providers
+ *   description: The providers managing API
+ */
 
 /**
  * @swagger
